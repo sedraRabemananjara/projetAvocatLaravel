b454edde497ee970e6e24860fa3f1ecb9d43496e
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2022-06-10 13:56:06 +0300

    Update ControllerUpdateAgenda.php

--- app/Http/Controllers/agenda/ControllerUpdateAgenda.php
+++ app/Http/Controllers/agenda/ControllerUpdateAgenda.php
@@ -5,27 +5,36 @@ namespace App\Http\Controllers\agenda;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\models\Agenda;
+use Illuminate\Support\Facades\DB;
 
 class ControllerUpdateAgenda extends Controller
 {
-    public function edit($idCourse){
-        $course= Agenda::find($idCourse);  
-        return view('modifierCourse', compact('course'));  
+    public function update($id)
+    {
+       request()->validate([
+            'idEnregistrement' => 'required',
+            'renvoi' => 'required',
+            'motif' => 'required',
+            'espaceConclusion' => 'required',
+            'idCourse' => 'required',
+            'dateTimeAgenda' => 'required',
+        ]);
+
+        $agenda = DB::table('agendas')
+                        ->where('idAgenda', $id)
+                        ->update([
+                            'idEnregistrement' => request('idEnregistrement'),
+                            'renvoi' => request('renvoi'),
+                            'motif' => request('motif'),
+                            'espaceConclusion' => request('espaceConclusion'),
+                            'idCourse' => request('idCourse'),
+                            'dateTimeAgenda' => request('dateTimeAgenda'),
+                        ]);
+
+        //return redirect()->route('modifier-echeance', ['id' => $id])->with('succes', 'Enregistrement mis à jour');
+
     }
     
 
-    public function update(Request $request, $idCourse)  
-    {  
-        //    
-        $agenda = Agenda::find();  
-         
-        $agenda->idEnregistrement=$request->input('idEnregistrement');
-        $agenda->renvoi=$request->input('renvoi');
-        $agenda->motif=$request->input('motif');
-        $agenda->idCourse=$request->input('idCourse');
-        $agenda->dateTimeAgenda=$request->input('dateTimeAgenda');
-
-        $agenda->save(); 
-        //return view('pageLister',['listecourses'=> course::all()]);
-    } 
+  
 }
