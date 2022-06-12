5f13bf0089363e32a8a4d78f9f75f0deec08ca4c
Manda Hariniaina <80709727+MandaHariniaian@users.noreply.github.com>
2022-06-12 20:24:01 +0300

    authentification

--- app/Http/Controllers/user/UserSelectController.php
+++ app/Http/Controllers/user/UserSelectController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserSelectController extends Controller
 {
+
+    public function select()
+    {
+        return Auth::user();
+    }
+
     public function selectAll()
     {
         return User::all();
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\user\UserLoginController;
 use App\Http\Controllers\user\UserSelectController;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Illuminate\Support\Facades\Auth;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,8 +12,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-Route::post('/login', [AccessTokenController::class, 'issueToken'])->middleware(['api-login', 'throttle']);
+Route::post('login', [AccessTokenController::class, 'issueToken'])
+    ->middleware(['api-login', 'throttle']);
 
-Route::get('/users', [UserSelectController::class, 'selectAll']);
+
+Route::middleware(['web', 'auth:api'])->group(function () {
+    Route::get('/users', [UserSelectController::class, 'selectAll']);
+
+    Route::get('/user', [UserSelectController::class, 'select']);
+
+    Route::get('/valider-token', function () {
+        return ['data' => true];
+    });
+});
 
 // Route::post('/login', [UserLoginController::class, 'login']);
