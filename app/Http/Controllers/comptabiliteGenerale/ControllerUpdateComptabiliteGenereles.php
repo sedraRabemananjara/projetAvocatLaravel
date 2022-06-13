8d68e003be9eed3f45396da4c5baf196ccb7ca95
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2022-06-13 12:11:27 +0300

    2

--- app/Http/Controllers/comptabiliteGenerale/ControllerUpdateComptabiliteGenereles.php
+++ app/Http/Controllers/comptabiliteGenerale/ControllerUpdateComptabiliteGenereles.php
@@ -4,8 +4,33 @@ namespace App\Http\Controllers\comptabiliteGenerale;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\models\ComptabiliteFrais;
+use Illuminate\Support\Facades\DB;
 
 class ControllerUpdateComptabiliteGenereles extends Controller
 {
-    //
+    public function update($id)
+    {
+       request()->validate([
+            'idEnregistrement' => 'required',
+            'coutActes' => 'required',
+            'fraisProcedure' => 'required',
+            'date' => 'required',
+            'entite' => 'required',
+            'especeRecu' => 'required',
+            'motif' => 'required',
+            'remarque' => 'required',
+        ]);
+
+        $ComptabiliteFrais = DB::table('comptabilite_frais')
+                        ->where('id_comptabilite_frais', $id)
+                        ->update([
+                            'idEnregistrement' => request('idEnregistrement'),
+                            'coutActes' => request('coutActes'),
+                            'fraisProcedure' => request('fraisProcedure'),
+                        ]);
+
+        //return redirect()->route('modifier-echeance', ['id' => $id])->with('succes', 'Enregistrement mis à jour');
+
+    }
 }
--- app/Http/Controllers/comptabiliteHonoraire/ControllerListerComptabiliteHonoraires.php
+++ app/Http/Controllers/comptabiliteHonoraire/ControllerListerComptabiliteHonoraires.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\comptabiliteHonoraire;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\models\ComptabiliteHonoraire;
 
 class ControllerListerComptabiliteHonoraires extends Controller
 {
-    //
+    public function getAllComptabiliteHonoraire(Request $req){
+        ComptabiliteHonoraire::all();
+        return view('pageListerComptabiliteHonoraire',['listeComptabiliteHonoraires'=> ComptabiliteHonoraire::all()]);
+    }
 }
