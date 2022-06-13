39ca53e5e58aa8cdd5e8065e307c97aee41b52f5
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2022-06-13 12:03:57 +0300

    first commit 13 juin

---
+++ app/Http/Controllers/comptabiliteGenerale/ControllerInsertComptabiliteGenereles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\comptabiliteGenerale;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControllerInsertComptabiliteGenereles extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/comptabiliteHonoraire/ControllerListerComptabiliteHonoraires.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\comptabiliteHonoraire;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControllerListerComptabiliteHonoraires extends Controller
+{
+    //
+}
